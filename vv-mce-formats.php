f706d5b6f925fb8f09e5e0383382fbdb2829b56b
Verdon Vaillancourt <verdonv@gmail.com>
2017-08-30 11:38:57 -0400

    minor tweak to get current

--- vv-mce-formats.php
+++ vv-mce-formats.php
@@ -3,27 +3,16 @@
 Plugin Name: Verdon's MCE Formats
 Plugin URI: http://verdon.ca/
 Description: A plugin to replace the wp formats menu in the MCE Editor with a custom one
-Version: 0.1
+Version: 0.2
 Author: Verdon Vaillancourt
 Author URI: http://verdon.ca/
 License: GPL2
 */
 
 	function vv_mce_mod( $init ) {
-
-		// example of how to customize the choices in the WP Format button
-		// $init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;' . $hi_format;
-		
-		// how to build an array of items to add or replace those in the MCE Style Select / Formats button
+		// build an array of items to add or replace those in the MCE Style Select / Formats drop-down
 		$style_formats = array (
 			array ( 'title' => 'Custom', 'items' => array( 
-				// array( 'title' => 'Bold text', 'inline' => 'b' ),
-				// array( 'title' => 'Red text', 'inline' => 'span', 'styles' => array( 'color' => '#ff0000' ) ),
-				// array( 'title' => 'Paragraph', 'block' => 'p' ),
-				// array( 'title' => 'Heading 2', 'block' => 'h2' ),
-				// array( 'title' => 'Heading 3', 'block' => 'h3' ),
-				// array( 'title' => 'Heading 4', 'block' => 'h4' ),
-				// array( 'title' => 'Hanging Indent', 'block' => 'p', 'styles' => array( 'margin-left' => '3em', 'text-indent' => '-3em' ) )
 				array( 'title' => 'Callout', 'block' => 'blockquote', 'wrapper' => true, 'classes' => 'vv_callout' ),
 				array( 'title' => 'Hanging Indent - Clear Floats', 'block' => 'p', 'classes' => 'vv_hanging_indent' ),
 				array( 'title' => 'Hanging Indent - No Clear', 'block' => 'p', 'classes' => 'vv_hanging_indent_noclear' )
@@ -32,7 +21,12 @@ License: GPL2
 		$init['style_formats'] = json_encode( $style_formats );
 
 		// whether to merge the above array with the default MCE Style Select, or replace it
-		$init['style_formats_merge'] = true;
+		global $wp_version;
+		if ($wp_version >= 4.7) {
+			$init['style_formats_merge'] = false;
+		} else {
+			$init['style_formats_merge'] = true;
+		}
 
 		return $init;
 	}
@@ -40,17 +34,26 @@ License: GPL2
 //	hook to do the above before the editor is initialized
 	add_filter('tiny_mce_before_init', 'vv_mce_mod');
 
-//	function to add the MCE Style Select button to the 2nd toolbar array
+//	function to add the MCE Style Select to the toolbar array
 	function vv_mce_add_buttons( $buttons ){
-		array_splice( $buttons, 1, 0, 'styleselect' );
-		// this will now remove the WP Formats button
-		unset($buttons[0]);
+		global $wp_version;
+		if ($wp_version >= 4.7) {
+			array_splice( $buttons, 0, 0, 'styleselect' );
+		} else {
+			array_splice( $buttons, 1, 0, 'styleselect' );
+			// this will now remove the WP Formats button
+			unset($buttons[0]);
+		}
 		return $buttons;
 	}
 
 //	hook to do the above
-	add_filter( 'mce_buttons_2', 'vv_mce_add_buttons' );
-
+	if ($wp_version >= 4.7) {
+		add_filter( 'mce_buttons_2', 'vv_mce_add_buttons' );
+	} else {
+		add_filter( 'mce_buttons_2', 'vv_mce_add_buttons' );
+	}
+	
 // function and hook to add css
 	function wptuts_styles_with_the_lot(){
 		// Register the style
